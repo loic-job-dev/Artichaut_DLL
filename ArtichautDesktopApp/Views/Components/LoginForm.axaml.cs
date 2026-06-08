54c4ff72e01fab7e1d865af814b55233d00e9164
loic-job-dev <loicjobard@hotmail.com>
2026-06-08 12:27:44 +0200

    added header and loginForm components

---
+++ ArtichautDesktopApp/Views/Components/LoginForm.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace ArtichautDesktopApp.Views.Components;
+
+public partial class LoginForm : UserControl
+{
+    public LoginForm()
+    {
+        InitializeComponent();
+    }
+}
