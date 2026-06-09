4f4572985d71220bd85a78efd2568d13ca2bddb5
loic-job-dev <loicjobard@hotmail.com>
2026-06-09 11:54:38 +0200

    navigation to CheckinView available, logout available

--- ArtichautDesktopApp/ViewModels/CheckinViewModel.cs
+++ ArtichautDesktopApp/ViewModels/CheckinViewModel.cs
@@ -1,6 +1,17 @@
+using ArtichautDesktopApp.Services;
+
 namespace ArtichautDesktopApp.ViewModels;
 
 public class CheckinViewModel : ViewModelBase
 {
+    private readonly INavigationService _navigation;
+
+    public SideMenuViewModel SideMenu { get; }
+
+    public CheckinViewModel(SideMenuViewModel sideMenu, INavigationService navigation)
+    {
+        SideMenu = sideMenu;
+        _navigation = navigation;
+    }
     
 }
